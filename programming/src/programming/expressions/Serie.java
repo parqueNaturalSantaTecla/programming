e4fc3b08071c0a7b031560a92649618c97e30cf1
Tamara <Tamara@192.168.0.11>
2019-06-16 14:57:24 +0200

    A. Clase Serie

---
+++ programming/src/programming/expressions/Serie.java
@@ -0,0 +1,27 @@
+package programming.expressions;
+
+public class Serie {
+
+	final int MAX = 9;
+
+	int condition(int number) {
+		return number; // en orden
+//		return number + 1; // en orden del 1 al 9
+//		return (MAX-1) - number; // en orden inverso del 0 al 8
+//		return number % 3; // serie de 0,1,2
+//		return number * 2; // de 2 en 2
+//		return (number + 1) * MAX; // la tabla del 9
+	}
+
+	public void exec() {
+		for (int i = 0; i < MAX; i++) {
+			System.out.println(condition(i));
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		new Serie().exec();
+	}
+
+}
