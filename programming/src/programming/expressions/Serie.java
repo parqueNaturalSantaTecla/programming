65e783bad80e0c92ccdc89fd3315a3bd780f0bee
Tamara <Tamara@192.168.0.11>
2019-06-18 12:32:10 +0200

    A. Nueva serie

--- programming/src/programming/expressions/Serie.java
+++ programming/src/programming/expressions/Serie.java
@@ -2,20 +2,21 @@ package programming.expressions;
 
 public class Serie {
 
-	final int MAX = 9;
+	final int MAX = 10;
 
-	int condition(int number) {
-		return number; // en orden
-//		return number + 1; // en orden del 1 al 9
-//		return (MAX-1) - number; // en orden inverso del 0 al 8
+	int getTerm(int number) {
+//		return number; // en orden
+//		return number + 1; // en orden del 1 al 10
+//		return (MAX-1) - number; // en orden inverso del 0 al 9
 //		return number % 3; // serie de 0,1,2
 //		return number * 2; // de 2 en 2
-//		return (number + 1) * MAX; // la tabla del 9
+//		return (number + 1) * MAX; // la tabla del 10
+		return ((MAX - number - 1) / (MAX / 2)) * number + (number / (MAX / 2)) * (MAX - number - 1);
 	}
 
 	void exec() {
 		for (int i = 0; i < MAX; i++) {
-			System.out.println(condition(i));
+			System.out.println(i + " - " + getTerm(i));
 		}
 		System.out.println();
 	}
