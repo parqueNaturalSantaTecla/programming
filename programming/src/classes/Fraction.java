6d8c8fda7fe7ca25ca01f6a19eda7764d72f94b2
Tamara <Tamara@192.168.0.11>
2019-06-25 13:30:06 +0200

    A. Creada clase Fraction

---
+++ programming/src/classes/Fraction.java
@@ -0,0 +1,59 @@
+package classes;
+
+public class Fraction {
+
+	int numerator;
+
+	int denominator;
+
+	Fraction() {
+	}
+
+	Fraction(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+
+	Fraction add(Fraction fraction1, Fraction fraction2) {
+		Fraction result = new Fraction();
+		result.numerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
+		result.denominator = fraction1.denominator * fraction2.denominator;
+		return result;
+	}
+
+	Fraction substract(Fraction fraction1, Fraction fraction2) {
+		Fraction result = new Fraction();
+		result.numerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator;
+		result.denominator = fraction1.denominator * fraction2.denominator;
+		return result;
+	}
+
+	Fraction multiply(Fraction fraction1, Fraction fraction2) {
+		return new Fraction(fraction1.numerator * fraction2.numerator, fraction1.denominator * fraction2.denominator);
+	}
+
+	Fraction inverse(Fraction fraction) {
+		return new Fraction(fraction.denominator, fraction.numerator);
+	}
+
+	Fraction divide(Fraction fraction1, Fraction fraction2) {
+		return this.multiply(fraction1, this.inverse(fraction2));
+	}
+
+	int getNumerator() {
+		return numerator;
+	}
+
+	void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+
+	int getDenominator() {
+		return denominator;
+	}
+
+	void setDenominator(int denominator) {
+		this.denominator = denominator;
+	}
+
+}
