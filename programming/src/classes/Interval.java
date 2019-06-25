1a90b325c143dd382d6c4cdaf27d7e3f3d810334
Tamara <Tamara@192.168.0.11>
2019-06-25 13:30:14 +0200

    A. Creada clase Interval

---
+++ programming/src/classes/Interval.java
@@ -0,0 +1,34 @@
+package classes;
+
+public class Interval {
+
+	private int min;
+
+	private int max;
+
+	Interval(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	boolean includes(int value) {
+		return this.min <= value && value <= this.max;
+	}
+
+	int getMin() {
+		return min;
+	}
+
+	void setMin(int min) {
+		this.min = min;
+	}
+
+	int getMax() {
+		return max;
+	}
+
+	void setMax(int max) {
+		this.max = max;
+	}
+
+}
